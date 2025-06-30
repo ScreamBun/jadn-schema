d4c9a7a5a47ed0249672186f69c99e4d02f994f7
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-06-30 13:49:18 -0400

    added comment to 2020 json schema validator

--- jadnschema/convert/schema/writers/json_schema/schema_validator.py
+++ jadnschema/convert/schema/writers/json_schema/schema_validator.py
@@ -4,7 +4,7 @@ def validate_schema(schema: dict)-> tuple[bool, str]:
     #TODO: Allow the ability to chose different Draft Validator versions
 
     try:
-        Draft202012Validator.check_schema(schema)
+        Draft202012Validator.check_schema(schema)  # NOTE: Keep an eye on this, we had to roll back to 2019 due to issues with 2020
         return True, "Schema is Valid"
 
     except ValidationError as e:
